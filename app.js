69146d5bf7f37c5c6cdeff1b891903c4c7ff362b
Cecilia Blankson Amponsah <amponsahcecilia77@gmail.com>
2022-08-11 14:52:50 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+//Declaring a variable
+//keyboard identifier/variableName = value
+let firstName = ("Cecilia");
+let lastName = ("Amponsah");
+let age = 25 ;
+let ismarried = ("false");
+let siblings = ("Ama" , "Adwoa" , "Star " );
+let other = {
+    favouritecolor: "pink",
+    favouritegame: "ludo",
+    favouritedress: "gown",
+    favoritefruit: "apple",
+
+}
+const fullName = firstName + " " + lastName;// concatenation 
+console.log(fullName);
+myself = `My name is ${firstName} ${lastName}`;
+console.log(myself);
+
+
+// STRINGS
+let  sentence = "this is a story of Cecilia Amponsah in a room"
+console.log(sentence.length);
+console.log(sentence.split("Ama"));
+console.log(sentence.replace("Ama" , "Amponsah"));
+console.log(sentence.toLowerCase());
+console.log(sentence.toUpperCase());
+console.log(sentence.startsWith("The"));
+console.log(sentence.endsWith("sport"));
+console.log(sentence.includes(""));
+
